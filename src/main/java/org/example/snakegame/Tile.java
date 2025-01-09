625c5c0df8cf4f82159ef33cd301f1887e128f5c
zerock <kangckck24@gmail.com>
2025-01-09 14:03:51 +0900

    Lotto

--- src/main/java/org/example/snakegame/Tile.java
+++ src/main/java/org/example/snakegame/Tile.java
@@ -2,13 +2,16 @@ package org.example.snakegame;
 
 
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @ToString
+@Setter
 public class Tile {
 
     private int num;
+    private String desc;
 
     public Tile(int num) {
         this.num = num;
